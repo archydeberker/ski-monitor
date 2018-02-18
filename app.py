d50a8451e5e3645bf653a6afc6c83b8417fc43f4
Archy de Berker <archy.deberker@gmail.com>
2018-02-17 21:27:46 -0500

    Setup full psotgres integration with API scripts

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+from dash.dependencies import Input, Output
 
 from db_utils import connect_to_db, get_table
 
@@ -11,16 +12,27 @@ server = app.server
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/' +
-    '5d1ea79569ed194d432e56108a04d188/raw/' +
-    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-    'gdp-life-exp-2007.csv')
-
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
+
+app.layout = html.Div([
+    dcc.Tabs(
+        tabs=[
+            {'label': 'Historic Data', 'value': 0},
+            {'label': 'Forecast Data', 'value': 1},
+        ],
+        value=0,
+        id='tabs'
+    ),
+    html.Div(id='tab-output')
+], style={
+    'width': '80%',
+    'fontFamily': 'Sans-Serif',
+    'margin-left': 'auto',
+    'margin-right': 'auto'
+})
 
 def base_graph(i):
 
@@ -53,39 +65,109 @@ def base_graph(i):
     )
 
 
-def postgres_graph(table_name):
+class PostgresGrapher():
 
-    conn = connect_to_db()
-    df = get_table(conn, table_name)
+    def __init__(self):
+        self.conn = connect_to_db()
 
-    return dcc.Graph(
-        id= table_name,
-        figure={
-            'data': [
-                go.Scatter(
-                    x=df[df['resort'] == i]['time'],
-                    y=df[df['resort'] == i]['measurement'],
-                    text=df[df['resort'] == i]['measurement'],
-                    mode='line',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df['resort'].unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'title': 'Date'},
-                yaxis={'title': 'Temperature'},
-                margin={'l': 100, 'b': 100, 't': 100, 'r': 100},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest',
-                title='Temperature graph'
-            )
-        }
-    )
+    def plot_lines(self, table_name, column, title):
 
+        df = get_table(self.conn, table_name).sort_values(by='timestamp')
+
+        return dcc.Graph(
+            id= '-'.join((table_name, column)),
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=df[df['location'] == i]['timestamp'],
+                        y=df[df['location'] == i][column],
+                        text=df[df['location'] == i][column],
+                        mode='line',
+                        opacity=0.7,
+                        marker={
+                            'size': 15,
+                            'line': {'width': 0.5, 'color': 'white'}
+                        },
+                        name=i
+                    ) for i in df['location'].unique()
+                ],
+                'layout': go.Layout(
+                    xaxis={'title': 'Date'},
+                    yaxis={'title': title},
+                    margin={'l': 100, 'b': 100, 't': 100, 'r': 100},
+                    legend={'x': 1, 'y': 1},
+                    hovermode='closest',
+                    title=title
+                )
+            }
+        )
+
+    def plot_area(self, table_name, column, title):
+
+        df = get_table(self.conn, table_name).sort_values(by='timestamp')
+
+        return dcc.Graph(
+            id= '-'.join((table_name, column)),
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=df[df['location'] == i]['timestamp'],
+                        y=df[df['location'] == i][column],
+                        text=df[df['location'] == i][column],
+                        mode='none',
+                        opacity=0.1,
+                        fill='tozeroy',
+                        name=i
+                    ) for i in df['location'].unique()
+                ],
+                'layout': go.Layout(
+                    xaxis={'title': 'Date'},
+                    yaxis={'title': title},
+                    margin={'l': 100, 'b': 100, 't': 100, 'r': 100},
+                    legend={'x': 1, 'y': 1},
+                    hovermode='closest',
+                    title=title
+                )
+            }
+        )
+
+    def plot_opposed(self, table_name, column1, column2, title):
+        ' Column1 will be plotted in positive, Column 2 in negative'
+
+        df = get_table(self.conn, table_name)
+
+        return dcc.Graph(
+            id= '-'.join((table_name, column1, column2)),
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=df[df['location'] == i]['timestamp'],
+                        y=df[df['location'] == i][column1],
+                        text=df[df['location'] == i][column1],
+                        mode='none',
+                        opacity=0.1,
+                        fill='tozeroy',
+                        name=i
+                    ) for i in df['location'].unique()] +
+                    [go.Scatter(
+                        x=df[df['location'] == i]['timestamp'],
+                        y=-df[df['location'] == i][column2],
+                        text=df[df['location'] == i][column2],
+                        mode='none',
+                        opacity=0.1,
+                        fill='tonexty',
+                        name=i
+                    ) for i in df['location'].unique()],
+                'layout': go.Layout(
+                    xaxis={'title': 'Date'},
+                    yaxis={'title': title},
+                    margin={'l': 100, 'b': 100, 't': 100, 'r': 100},
+                    legend={'x': 1, 'y': 1},
+                    hovermode='closest',
+                    title=title
+                )
+            }
+        )
 
 header_div = html.H1(
         children='Welcome to Ski Monitor',
@@ -95,10 +177,24 @@ header_div = html.H1(
         }
     )
 
-#graph_divs = html.Div([base_graph(1), base_graph(2), base_graph(3), base_graph(4)], style={'columnCount': 2})
-graph_divs = html.Div([postgres_graph('temperatures')])
 
-app.layout = html.Div([header_div, graph_divs])
+pg = PostgresGrapher()
+#graph_divs = html.Div([base_graph(1), base_graph(2), base_graph(3), base_graph(4)], style={'columnCount': 2})
+@app.callback(Output('tab-output', 'children'), [Input('tabs', 'value')])
+def display_content(value):
+    if value==0:
+        graph_divs = html.Div([pg.plot_lines('current', 'temp_c_max', 'Max Temperature (C)'),
+                           pg.plot_lines('current', 'windspeed_mph', 'Windspeed (Mph)')],
+                           style={'columnCount': 2})
+
+        return html.Div([graph_divs, html.Div(pg.plot_opposed('current', 'snow_mm', 'rain_mm', '+ Snow (mm), - Rain(mm)'))])
+    else:
+        graph_divs = html.Div([pg.plot_lines('forecasts', 'temp_c_max', 'Max Temperature (C)'),
+                           pg.plot_lines('forecasts', 'windspeed_mph', 'Windspeed (Mph)')],
+                           style={'columnCount': 2})
+
+        return html.Div([graph_divs, html.Div(pg.plot_opposed('forecasts', 'snow_mm', 'rain_mm', '+ Snow (mm), - Rain(mm)'))])
+
 
 if __name__ == '__main__':
     app.run_server()
--- db_utils.py
+++ db_utils.py
@@ -3,6 +3,12 @@ from urllib import parse
 import pandas as pd
 import psycopg2
 import os
+import json
+import requests
+import datetime
+
+from psycopg2.extensions import AsIs
+
 
 def connect_to_db():
 
@@ -25,3 +31,101 @@ def get_table(conn, table_name):
 
     return df
 
+def get_current_weather(loc_dict):
+
+    data_dict = {}
+    for location in loc_dict.keys():
+
+        r = requests.get('http://api.weatherunlocked.com/api/forecast/%1.2f,%1.2f?app_id=6765a93c&app_key=51b7b6cb5cf11b77b78d84d8f5c44845'%(loc_dict[location]))
+
+        result = json.loads(r.text)
+
+        data_dict[location] = {'Windspeed_Mph':result['Days'][0]['windspd_max_mph'],
+                               'Rain_mm': result['Days'][0]['rain_total_mm'],
+                               'Snow_mm': result['Days'][0]['snow_total_mm'],
+                               'Temp_C_Max': result['Days'][0]['temp_max_c'],
+                               'Temp_C_Min': result['Days'][0]['temp_min_c'],
+                               'Timestamp': datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
+                              }
+
+    return data_dict
+
+def get_forecast_weather(loc_dict):
+
+    data_dict = {}
+
+    for location in loc_dict.keys():
+
+        r = requests.get('http://api.weatherunlocked.com/api/forecast/%1.2f,%1.2f?app_id=6765a93c&app_key=51b7b6cb5cf11b77b78d84d8f5c44845'%(loc_dict[location]))
+
+        result = json.loads(r.text)
+        tomorrow = result['Days'][1]['Timeframes']
+
+        data_dict[location] ={}
+        for i in range(8):
+            timestamp = (datetime.datetime.strptime(result['Days'][1]['date'],'%d/%m/%Y')
+                         +datetime.timedelta(hours=int(tomorrow[i]['time'])/100)).strftime('%Y-%m-%d %H:%M')
+            data_dict[location][timestamp]= {'Windspeed_Mph':tomorrow[i]['windspd_mph'],
+                                                       'Rain_mm': tomorrow[i]['rain_mm'],
+                                                       'Snow_mm': tomorrow[i]['snow_mm'],
+                                                       'Temp_C_Max': tomorrow[i]['temp_c'],
+                                                       'Temp_C_Min': tomorrow[i]['temp_c'],
+
+                              }
+
+
+    return data_dict
+
+def add_row(data, table_name):
+
+    columns = data.keys()
+    values = [data[column] for column in columns]
+
+    insert_statement = 'INSERT into ' + table_name + ' (%s) values %s'
+
+    cursor=conn.cursor()
+    try:
+        cursor.execute(insert_statement, (AsIs(','.join(columns)), tuple(values)))
+        conn.commit()
+    except psycopg2.IntegrityError:
+        print('Skipping value already in table')
+        conn.rollback()
+
+
+def add_forecasts(forecast_dict):
+
+    for location in forecast_dict.keys():
+
+        for timestamp in forecast_dict[location]:
+            data = forecast_dict[location][timestamp]
+            data['Timestamp'] = timestamp
+            data['Location'] = location
+
+            add_row(data, 'forecasts')
+
+def add_current(current_dict):
+
+    for location in forecast_dict.keys():
+
+        data = current_dict[location]
+        data['Location'] = location
+
+        add_row(data, 'current')
+
+
+if __name__ == '__main__':
+
+    loc_dict = {'Jay Peak':(44.9649,-72.4602),
+            'Mt Sutton':(45.1047, -72.5618),
+            'Mt Tremblant': (46.1185,-74.5962),
+            'St Anne': (47.0754,-70.9049),
+            'Le Massif': (47.2848,-70.5697),
+           }
+
+    conn = connect_to_db()
+
+    forecast_dict = get_forecast_weather(loc_dict)
+    add_forecasts(forecast_dict)
+
+    current_dict = get_current_weather(loc_dict)
+    add_current(current_dict)
