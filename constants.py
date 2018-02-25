0860f17c0b9b84f9b8245add1b6e222d4a7072b0
Archy de Berker <archy.deberker@gmail.com>
2018-02-25 13:40:25 -0500

    Use OrderedDict to get consistent colouring of resorts

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 loc_dict = {'Jay Peak':(44.9367723, -72.525403),  # Jay Peak Summit https://goo.gl/maps/q1YWigKsBd62
             'Mt Sutton':(45.0894197, -72.5603979),  # Mt Sutton Chalet 840 https://goo.gl/maps/ZYpM4WYmuM92
             'Mt Tremblant': (46.2124932, -74.5620593),  # Mt Tremblant Summit https://goo.gl/maps/K6QzKPPZjKF2
@@ -5,6 +7,8 @@ loc_dict = {'Jay Peak':(44.9367723, -72.525403),  # Jay Peak Summit https://goo.
             'Le Massif': (47.2816835,-70.6107957),  # Le Massif Resort (at top) https://goo.gl/maps/q7sTMfiJ1zP2
             'Stowe': (44.5310787, -72.8070048)  # Mt Mansfield Summit https://goo.gl/maps/MqwdMnXRPyo
             }
+
+loc_dict = OrderedDict(sorted(loc_dict.items(), key=lambda t: t[0]))
 
 url_dict = {'Jay Peak': 'https://www.onthesnow.com/vermont/jay-peak/skireport.html',
             'Mt Sutton': 'https://www.onthesnow.ca/quebec/mont-sutton/skireport.html',
@@ -14,6 +18,8 @@ url_dict = {'Jay Peak': 'https://www.onthesnow.com/vermont/jay-peak/skireport.ht
             'Stowe': 'https://www.onthesnow.com/vermont/stowe-mountain-resort/skireport.html',
             }
 
+url_dict = OrderedDict(sorted(url_dict.items(), key=lambda t: t[0]))
 
 for key in loc_dict.keys():
     assert url_dict.get(key) is not None
+
